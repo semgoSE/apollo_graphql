9c1e8f77be77b3add24a50eb52542830a4b297a9
BuildTools <semgo@gmail.com>
2021-05-11 23:53:02 +0300

    add dist

---
+++ dist/data/Book.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Book = /** @class */ (function () {
+    function Book(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+    return Book;
+}());
+exports["default"] = Book;
---
+++ dist/data/User.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var User = /** @class */ (function () {
+    function User(id, email, first_name, last_name, vacation) {
+        this.id = id;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.vacation = vacation;
+    }
+    return User;
+}());
+exports["default"] = User;
